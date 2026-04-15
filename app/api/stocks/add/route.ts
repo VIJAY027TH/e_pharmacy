1de43793d801f8a5f138c9fd10aa43a32fbcd7a7
VIJAY)@027TH <vijayethicalhacker027@gmail.com>
2026-04-15 19:31:46 +0530

    fix result error

--- app/api/stocks/add/route.ts
+++ app/api/stocks/add/route.ts
@@ -6,12 +6,12 @@ export async function POST(req: Request) {
   const db = await getDB();
 
   // Step 1: Add to Pending (orders table)
-  await db.run(
-  "INSERT INTO orders (name, type, quantity, user, status, created_at) VALUES (?, ?, ?, ?, ?, ?)",
-  [name, type, quantity, "admin", "Pending", Date.now()]
+  const result = await db.run(
+  "INSERT INTO stocks (type, name, quantity) VALUES (?, ?, ?)",
+  [type, name, quantity]
 );
 
-  const insertedId = result.lastID;
+const insertedId = result.lastID;
 
   return Response.json({ message: "Stock will be added in 30 sec" });
 }
