a13ced68aa85c4ae7097de2ae378ad1989ed7891
VIJAY)@027TH <vijayethicalhacker027@gmail.com>
2026-04-15 20:02:42 +0530

    fix db for better-sqlite3

--- lib/db.ts
+++ lib/db.ts
@@ -1,58 +1,54 @@
 import Database from "better-sqlite3";
-import bcrypt from "bcrypt";
 
 let db: any = null;
 
-export async function getDB() {
+export function getDB() {
   if (db) return db;
 
   db = new Database("./database.sqlite");
 
-  //  Auto create table if not exists
-  await db.exec(`
+  // CREATE TABLES
+  db.exec(`
     CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  email TEXT UNIQUE,
-  password TEXT,
-  role TEXT DEFAULT 'user'
-)
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      email TEXT UNIQUE,
+      password TEXT,
+      role TEXT DEFAULT 'user'
+    )
   `);
 
-  await db.exec(`
-  CREATE TABLE IF NOT EXISTS stocks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    type TEXT,
-    name TEXT,
-    quantity INTEGER
-  )
-`);
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS stocks (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      type TEXT,
+      name TEXT,
+      quantity INTEGER
+    )
+  `);
 
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS orders (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      type TEXT,
+      quantity INTEGER,
+      user TEXT,
+      status TEXT DEFAULT 'Pending',
+      created_at INTEGER
+    )
+  `);
 
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS orders (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-   name TEXT NOT NULL,
-    type TEXT,
-    quantity INTEGER,
-    user TEXT,
-    status TEXT DEFAULT 'Pending',
-    created_at INTEGER
-  )
-`);
+  // CHECK ADMIN
+  const admin = db
+    .prepare("SELECT * FROM users WHERE email = ?")
+    .get("admin@gmail.com");
 
-  // Auto create admin if not exists
- const admin = await db.get(
-  "SELECT * FROM users WHERE email = ?",
-  ["admin@gmail.com"]
-);
-
-if (!admin) {
-  await db.run(
-    "INSERT INTO users (email, password, role) VALUES (?, ?, ?)",
-    ["admin@gmail.com", "admin123", "admin"]
-  );
-}
+  if (!admin) {
+    db.prepare(
+      "INSERT INTO users (email, password, role) VALUES (?, ?, ?)"
+    ).run("admin@gmail.com", "admin123", "admin");
+  }
 
   return db;
 }
